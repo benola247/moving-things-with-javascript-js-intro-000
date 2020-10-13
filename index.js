895e5dc1c30d6cdc3ed8e98331daa2718e2259d3
Ebenezer Olaleye <benola247@yahoo.com>
2020-10-13 20:33:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var dodger = document.getElementById('dodger')
 function moveDodgerRight() {
   var leftNumbers = dodger.style.right.replace('px', '')
   var right = parseInt(rightNumbers, 10)
- 
+
   if (right > 0) {
     dodger.style.right = `${right - 1}px`
   }
